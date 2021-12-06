b05acffd869c67ddf1211a74b36a8a94ca6d3561
jinxing <jinxing.liu@verystar.net>
2021-12-06 18:29:22 +0800

    feat: compare

--- app/controllers/RedisController.php
+++ app/controllers/RedisController.php
@@ -10,17 +10,20 @@ class RedisController
     public function actionIndex()
     {
         $cluster = new \RedisCluster('redis-cluster', [
+            '127.0.0.1:6371',
             '127.0.0.1:6372',
             '127.0.0.1:6373',
             '127.0.0.1:6374',
             '127.0.0.1:6375',
             '127.0.0.1:6376',
-            '127.0.0.1:6377',
-        ], null, null, null, '6aHjkErH');
+        ]);
 
-        $keys = $cluster->keys('*');
+        for ($i = 0; $i < 100000; $i++) {
+            $cluster->set('key_' . $i, $i);
+        }
+
         success([
-            'count' => count($keys),
+            // 'count' => count($keys),
             'value' => $cluster->get('key_80477'),
         ]);
     }
--- public/socket.php
+++ public/socket.php
@@ -1,1 +1,23 @@
 <?php
+
+use Swoole\WebSocket\Server;
+
+$ws = new Server('0.0.0.0', 9502);
+
+//监听WebSocket连接打开事件
+$ws->on('Open', function ($ws, $request) {
+    $ws->push($request->fd, "hello, welcome\n" . $request->fd);
+});
+
+// 监听WebSocket消息事件
+$ws->on('Message', function ($ws, $frame) {
+    echo "Message: $frame->data\n";
+    $ws->push($frame->fd, "server: $frame->data");
+});
+
+// 监听WebSocket连接关闭事件
+$ws->on('Close', function ($ws, $fd) {
+    echo "client-$fd is closed\n";
+});
+
+$ws->start();
